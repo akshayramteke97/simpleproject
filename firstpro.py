5b4c674d9e83077f27e5306227649c2e6ea4bff9
Akshay <akshayramteke97@gmail.com>
2022-10-11 22:26:02 +0530

    first commit

---
+++ firstpro.py
@@ -0,0 +1,12 @@
+from os import sep
+from tkinter import N
+from turtle import end_fill
+
+
+#akki = 'Askhay is good boy' ; jai = 'BHAVESH IS CHURPYA' ; bhalu = 'sahil is hard working man'
+#len(jai)
+#getup = jai.lower()
+#print(getup)
+num = 'programming'
+concatination = 'I love to do ' + num + ' whenever i have free time'
+print(concatination) 
---
+++ listrange.py
@@ -0,0 +1,19 @@
+#num = list(range(0,11))
+#print(list(range(0,11)))
+#print(list(range(0,11,2))) #PRINT EVEN NUMBER
+#print(list(range(-70,50,10))) #FROM NEGATIVE VALUE TO POSTIVE
+
+#num = [2,4,6,8,4,2]
+#num.append(10) #for adding number at last of list
+#num.clear() #for clearing all element from list
+#num.pop() #for deleting last added number in list
+#num.insert(3,100) #for adding any element into list at specific space
+num1 = ['golu','bhalu','jai']
+#num1[0]='sahil'
+#num1.append('kalu')
+#num1.insert(1,"sahil")
+num2 = ['akshay','bhavesh']
+num1.append(num2)
+num1[3].insert(1,['shashank'])
+print(num1[3][1][0])
+
